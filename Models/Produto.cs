fc875c0506faa63d5aedc30e50bdc9d88e51e9b7
Felipe Baz <fbazmitsuishi@gmail.com>
2020-07-17 19:14:27 -0300

    Projeto Web Application

---
+++ Models/Produto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CursoMVC.Models
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Descrição")]
+        [Required(ErrorMessage = "o campo descrição é obrigatório")]
+        public string Descricao { get; set; }
+
+        [Required(ErrorMessage = "o campo Quantidade é obrigatório")]
+        [Range(1, 100, ErrorMessage ="Valor fora da faixa aceitavel")]
+        public int Quantidade { get; set; }
+
+        [Display(Name = "Preço")]
+        [Required(ErrorMessage = "o campo Preço é obrigatório")]
+        public int Preco { get; set; }
+
+        [Display(Name = "Categoria Id")]
+        public int CategoriaId { get; set; }
+        public  Categoria Categoria{ get; set; }
+    }
+}
---
+++ obj/Release/netcoreapp3.1/PubTmp/EFSQLScripts/CursoMVC.Models.Context.sql
@@ -0,0 +1,60 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20200717150438_InitialCreate')
+BEGIN
+    CREATE TABLE [categorias] (
+        [Id] int NOT NULL IDENTITY,
+        [Descricao] nvarchar(max) NULL,
+        CONSTRAINT [PK_categorias] PRIMARY KEY ([Id])
+    );
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20200717150438_InitialCreate')
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20200717150438_InitialCreate', N'3.1.6');
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20200717170938_TabelaProduto')
+BEGIN
+    CREATE TABLE [Produtos] (
+        [Id] int NOT NULL IDENTITY,
+        [Descricao] nvarchar(max) NULL,
+        [Quantidade] int NOT NULL,
+        [Preco] int NOT NULL,
+        [CategoriaId] int NOT NULL,
+        CONSTRAINT [PK_Produtos] PRIMARY KEY ([Id]),
+        CONSTRAINT [FK_Produtos_categorias_CategoriaId] FOREIGN KEY ([CategoriaId]) REFERENCES [categorias] ([Id]) ON DELETE CASCADE
+    );
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20200717170938_TabelaProduto')
+BEGIN
+    CREATE INDEX [IX_Produtos_CategoriaId] ON [Produtos] ([CategoriaId]);
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20200717170938_TabelaProduto')
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20200717170938_TabelaProduto', N'3.1.6');
+END;
+
+GO
+
